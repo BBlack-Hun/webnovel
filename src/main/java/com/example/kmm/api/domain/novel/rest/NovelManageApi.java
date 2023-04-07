1a3660979b78c48dc2da06db1f5edc39aae892f4
BBlack-Hun <h1103j@kangwon.ac.kr>
2023-04-07 21:50:08 +0900

    feat: [추가] 더미 api 추가
    개발을 위한 임시적으로 만들어 놓음.

--- src/main/java/com/example/kmm/api/domain/novel/rest/NovelManageApi.java
+++ src/main/java/com/example/kmm/api/domain/novel/rest/NovelManageApi.java
@@ -1,6 +1,23 @@
 package com.example.kmm.api.domain.novel.rest;
 
+import com.example.kmm.api.domain.novel.dto.request.NovelCoverRequest;
+import com.example.kmm.api.domain.novel.dto.request.NovelEpisodeRequest;
+import com.example.kmm.api.global.common.res.JsonObjectResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,11 +34,52 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/novel-manager")
 public class NovelManageApi {
 
-    // 소설 등록
+    // 소설 표지 등록
+    @PostMapping("/add")
+    public String addNovelCover(@RequestBody NovelCoverRequest novelCoverRequest)
+        throws JsonProcessingException {
+
+        return JsonObjectResponse.convert("success", true);
+    }
 
     // 소설 작성
+    @PostMapping("{coverId}/add")
+    public String addNovelEpisode(
+        @PathVariable Long coverId,
+        @RequestBody NovelEpisodeRequest episodeRequest
+    ) throws JsonProcessingException {
+
+        return JsonObjectResponse.convert("success", true);
+    }
 
     // 소설 조회
+    @GetMapping("")
+    public String getNovelCoverList(
+        @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) throws JsonProcessingException {
+        List<Map<String, Object>> novelList = new ArrayList<>();
+        return JsonObjectResponse.convert("novel", novelList);
+    }
+
+    // 각 소설의 각 챕터 조회
+    @GetMapping("")
+    public String getNovelEpisodeList(
+        @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) throws JsonProcessingException {
+        List<Map<String, Object>> novelList = new ArrayList<>();
+        return JsonObjectResponse.convert("novel", novelList);
+    }
 
     // 소설 삭제
+    @DeleteMapping("")
+    public String deleteNovelCover(String novelId) throws JsonProcessingException {
+        return JsonObjectResponse.convert("success", true);
+    }
+
+    // 소설 에피소드 삭제
+    @DeleteMapping("")
+    public String deleteNovelEpisode(String novelId, String episodeId)
+        throws JsonProcessingException {
+        return JsonObjectResponse.convert("success", true);
+    }
 }
