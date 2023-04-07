815323ae4f5936d9804b5b4d7ca4f6bdf07f7d0d
BBlack-Hun <h1103j@kangwon.ac.kr>
2023-04-07 21:42:53 +0900

    feat: [추가] 소설관리 entity 도메인(커버, 에피소드, 내용) 추가
    임시로 설계한 소설 Entity 이다.

---
+++ src/main/java/com/example/kmm/api/domain/novel/domain/NovelEpisodeContents.java
@@ -0,0 +1,49 @@
+package com.example.kmm.api.domain.novel.domain;
+
+import com.example.kmm.api.domain.user.dto.AuditingFields;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * NovelEpisodeContents class.
+ *
+ * <p>
+ * date: 2023-04-07
+ * </p>
+ *
+ * @author : 김정훈 파트장
+ * @version : V1.0.0
+ */
+@Getter
+@ToString(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+    name = "novel_episode_contents",
+    indexes = {
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+    })
+@Entity
+public class NovelEpisodeContents extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter
+    @ManyToOne(optional = false)
+    private NovelEpisode novelEpisode; // 소설 커버
+
+    private String contents;
+
+
+}
